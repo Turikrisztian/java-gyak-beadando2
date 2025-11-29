99615e8909082b7ed84b432d20d4e6e8eaf4a08c
Turikrisztian <148880706+Turikrisztian@users.noreply.github.com>
2025-11-29 22:31:36 +0100

    1

--- src/main/java/com/example/beadando/repository/TargyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.beadando.repository;
-
-import com.example.beadando.entity.Targy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TargyRepository extends JpaRepository<Targy, Long> {
-}
